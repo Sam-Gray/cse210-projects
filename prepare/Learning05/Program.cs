f5441baa220242fb7f0cb5b450533a7a302c2575
sam-gray <rogueone32678@gmail.com>
2023-12-02 14:59:28 -0700

    W05 Prepare Complete!

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("Red", 5.0);
+        Rectangle rectangle = new Rectangle("Blue", 4.0, 6.0);
+        Circle circle = new Circle("Green", 3.0);
+
+        DisplayShapeInfo(square);
+        DisplayShapeInfo(rectangle);
+        DisplayShapeInfo(circle);
+    }
+
+    static void DisplayShapeInfo(Shape shape)
+    {
+        Console.WriteLine($"Shape Color: {shape.GetColor()}");
+        Console.WriteLine($"Shape Area: {shape.GetArea()}");
+        Console.WriteLine();
     }
 }
