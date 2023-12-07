f9cc1e2c0e48c3a22c18852180ec40ce41e20238
sam-gray <rogueone32678@gmail.com>
2023-12-06 23:35:21 -0700

    Final Project Milestone Complete!

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,90 @@
 using System;
+using System.Collections.Generic;
+
+class Comment
+{
+    public string CommenterName { get; set; }
+    public string CommentText { get; set; }
+
+    public Comment(string commenterName, string commentText)
+    {
+        CommenterName = commenterName;
+        CommentText = commentText;
+    }
+}
+
+class Video
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int LengthSeconds { get; set; }
+    private List<Comment> Comments { get; set; }
+
+    public Video(string title, string author, int lengthSeconds)
+    {
+        Title = title;
+        Author = author;
+        LengthSeconds = lengthSeconds;
+        Comments = new List<Comment>();
+    }
+
+    public void AddComment(string commenterName, string commentText)
+    {
+        Comment comment = new Comment(commenterName, commentText);
+        Comments.Add(comment);
+    }
+
+    public int GetNumberOfComments()
+    {
+        return Comments.Count;
+    }
+
+    public void DisplayVideoInfo()
+    {
+        Console.Clear(); // Clear the console for each video
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Author: {Author}");
+        Console.WriteLine($"Length (seconds): {LengthSeconds}");
+        Console.WriteLine($"Number of Comments: {GetNumberOfComments()}");
+
+        if (GetNumberOfComments() > 0)
+        {
+            Console.WriteLine("\nComments:");
+            foreach (Comment comment in Comments)
+            {
+                Console.WriteLine($" - {comment.CommenterName}: {comment.CommentText}");
+            }
+        }
+
+        Console.WriteLine("\nPress Enter to view the next video...");
+        Console.ReadLine();
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        // Creating videos and adding comments
+        Video video1 = new Video("Introduction to C#", "C# Guru", 300);
+        video1.AddComment("Coder123", "Great tutorial!");
+        video1.AddComment("TechEnthusiast", "C# is amazing!");
+
+        Video video2 = new Video("Web Development Basics", "WebWizard", 500);
+        video2.AddComment("DevNewbie", "This is helpful, thanks!");
+        video2.AddComment("CodeMaster", "Web development is cool.");
+
+        Video video3 = new Video("Machine Learning Explained", "MLExpert", 600);
+        video3.AddComment("DataScienceFan", "Fascinating concepts!");
+        video3.AddComment("MLNewcomer", "Learning a lot from this.");
+
+        // Putting videos in a list
+        List<Video> videoList = new List<Video> { video1, video2, video3 };
+
+        // Displaying video information and comments
+        foreach (Video video in videoList)
+        {
+            video.DisplayVideoInfo();
+        }
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,195 @@
 using System;
+using System.Collections.Generic;
+
+class Address
+{
+    private string street;
+    private string city;
+    private string state;
+    private string country;
+
+    public Address(string street, string city, string state, string country)
+    {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+    }
+
+    public bool IsInUSA()
+    {
+        return country.ToLower() == "usa";
+    }
+
+    public string GetFullAddress()
+    {
+        return $"{street}\n{city}, {state}, {country}";
+    }
+}
+
+class Customer
+{
+    private string name;
+    private Address address;
+
+    public Customer(string name, Address address)
+    {
+        this.name = name;
+        this.address = address;
+    }
+
+    public bool IsInUSA()
+    {
+        return address.IsInUSA();
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public Address GetAddress()
+    {
+        return address;
+    }
+}
+
+class Product
+{
+    private string name;
+    private string productId;
+    private double price;
+    private int quantity;
+
+    public Product(string name, string productId, double price, int quantity)
+    {
+        this.name = name;
+        this.productId = productId;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public double GetTotalPrice()
+    {
+        return price * quantity;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public string GetProductId()
+    {
+        return productId;
+    }
+}
+
+class Order
+{
+    private List<Product> products;
+    private Customer customer;
+
+    public Order(Customer customer)
+    {
+        this.customer = customer;
+        this.products = new List<Product>();
+    }
+
+    public void AddProduct(Product product)
+    {
+        products.Add(product);
+    }
+
+    public double CalculateTotalCost()
+    {
+        double totalCost = 0;
+
+        foreach (Product product in products)
+        {
+            totalCost += product.GetTotalPrice();
+        }
+
+        // Adding shipping cost based on customer location
+        if (customer.IsInUSA())
+        {
+            totalCost += 5;
+        }
+        else
+        {
+            totalCost += 35;
+        }
+
+        return totalCost;
+    }
+
+    public string GetPackingLabel()
+    {
+        string packingLabel = "";
+
+        foreach (Product product in products)
+        {
+            packingLabel += $"{product.GetName()}, Product ID: {product.GetProductId()}\n";
+        }
+
+        return packingLabel;
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"{customer.GetName()}\n{customer.GetAddress().GetFullAddress()}";
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        Console.Clear(); // Clear the console
+
+        // Creating addresses
+        Address address1 = new Address("123 Main St", "Cityville", "CA", "USA");
+        Address address2 = new Address("456 Oak St", "Townsville", "NY", "Canada");
+        Address address3 = new Address("789 Pine St", "Villagetown", "TX", "USA");
+
+        // Creating customers
+        Customer customer1 = new Customer("John Doe", address1);
+        Customer customer2 = new Customer("Jane Smith", address2);
+        Customer customer3 = new Customer("Alice Johnson", address3);
+
+        // Creating products
+        Product product1 = new Product("Widget", "W123", 10.99, 2);
+        Product product2 = new Product("Gadget", "G456", 24.99, 1);
+        Product product3 = new Product("Doodad", "D789", 15.99, 3);
+        Product product4 = new Product("Thingamajig", "T101", 19.99, 1);
+
+        // Creating orders
+        Order order1 = new Order(customer1);
+        order1.AddProduct(product1);
+        order1.AddProduct(product2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(product3);
+        order2.AddProduct(product4);
+
+        Order order3 = new Order(customer3);
+        order3.AddProduct(product1);
+        order3.AddProduct(product3);
+        order3.AddProduct(product4);
+
+        // Displaying results for each order
+        DisplayOrderDetails(order1);
+        DisplayOrderDetails(order2);
+        DisplayOrderDetails(order3);
+    }
+
+    static void DisplayOrderDetails(Order order)
+    {
+        Console.WriteLine($"Packing Label:\n{order.GetPackingLabel()}");
+        Console.WriteLine($"Shipping Label:\n{order.GetShippingLabel()}\n");
+        Console.WriteLine($"Total Cost: ${order.CalculateTotalCost():0.00}\n");
+
+        Console.WriteLine("Press Enter to view the next order...");
+        Console.ReadLine();
     }
 }
