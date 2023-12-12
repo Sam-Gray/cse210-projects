86d7e6ce44bff84f8685b7bf04c173e1739c8eec
sam-gray <rogueone32678@gmail.com>
2023-12-12 11:26:43 -0700

    Final Project Done!!!

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,9 +1,177 @@
 using System;
 
+// Address class to encapsulate address details
+class Address
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string ZipCode { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Street}, {City}, {State} {ZipCode}";
+    }
+}
+
+// Base Event class
+class Event
+{
+    private string title;
+    private string description;
+    private DateTime date;
+    private TimeSpan time;
+    private Address address;
+
+    public Event(string title, string description, DateTime date, TimeSpan time, Address address)
+    {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.time = time;
+        this.address = address;
+    }
+
+    public string GetStandardDetails()
+    {
+        return $"{title}\n{description}\nDate: {date.ToShortDateString()}\nTime: {time}\nAddress: {address}";
+    }
+
+    public virtual string GetFullDetails()
+    {
+        return GetStandardDetails();
+    }
+
+    public string GetShortDescription()
+    {
+        return $"{GetType().Name} - {title}\nDate: {date.ToShortDateString()}";
+    }
+}
+
+// Lecture class derived from Event
+class Lecture : Event
+{
+    private string speaker;
+    private int capacity;
+
+    public Lecture(string title, string description, DateTime date, TimeSpan time, Address address, string speaker, int capacity)
+        : base(title, description, date, time, address)
+    {
+        this.speaker = speaker;
+        this.capacity = capacity;
+    }
+
+    public override string GetFullDetails()
+    {
+        return $"{base.GetFullDetails()}\nSpeaker: {speaker}\nCapacity: {capacity}";
+    }
+}
+
+// Reception class derived from Event
+class Reception : Event
+{
+    private string rsvpEmail;
+
+    public Reception(string title, string description, DateTime date, TimeSpan time, Address address, string rsvpEmail)
+        : base(title, description, date, time, address)
+    {
+        this.rsvpEmail = rsvpEmail;
+    }
+
+    public override string GetFullDetails()
+    {
+        return $"{base.GetFullDetails()}\nRSVP Email: {rsvpEmail}";
+    }
+}
+
+// OutdoorGathering class derived from Event
+class OutdoorGathering : Event
+{
+    private string weatherStatement;
+
+    public OutdoorGathering(string title, string description, DateTime date, TimeSpan time, Address address, string weatherStatement)
+        : base(title, description, date, time, address)
+    {
+        this.weatherStatement = weatherStatement;
+    }
+
+    public override string GetFullDetails()
+    {
+        return $"{base.GetFullDetails()}\nWeather: {weatherStatement}";
+    }
+}
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        do
+        {
+            Console.Clear(); // Clear the console before displaying the menu
+
+            // Create instances of each event type
+            Address address1 = new Address { Street = "123 Main St", City = "Cityville", State = "Stateville", ZipCode = "12345" };
+            Lecture lectureEvent = new Lecture("Interesting Lecture", "A fascinating talk about something", DateTime.Now, new TimeSpan(14, 0, 0), address1, "John Doe", 100);
+
+            Address address2 = new Address { Street = "456 Oak St", City = "Townsville", State = "Stateton", ZipCode = "54321" };
+            Reception receptionEvent = new Reception("Networking Reception", "An evening of networking and socializing", DateTime.Now.AddDays(7), new TimeSpan(18, 30, 0), address2, "rsvp@example.com");
+
+            Address address3 = new Address { Street = "789 Pine St", City = "Villageville", State = "Townston", ZipCode = "98765" };
+            OutdoorGathering outdoorEvent = new OutdoorGathering("Summer Picnic", "Enjoy a day outdoors with friends and family", DateTime.Now.AddDays(14), new TimeSpan(12, 0, 0), address3, "Sunny with a chance of clouds");
+
+            // Display menu to let the user choose an event
+            Console.WriteLine("Choose an Event:");
+            Console.WriteLine("1. Lecture Event");
+            Console.WriteLine("2. Reception Event");
+            Console.WriteLine("3. Outdoor Gathering Event");
+            Console.WriteLine("4. Exit");
+
+            // Get user input for event choice
+            int choice;
+            do
+            {
+                Console.Write("Enter your choice (1-4): ");
+            } while (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 4);
+
+            if (choice == 4)
+            {
+                // Exit the program if the user chooses to exit
+                break;
+            }
+
+            Console.Clear(); // Clear the console before displaying the marketing messages
+
+            // Display marketing messages based on user's choice
+            Console.WriteLine("=====================================");
+            Console.WriteLine("         EVENT MARKETING MESSAGES     ");
+            Console.WriteLine("=====================================\n");
+
+            switch (choice)
+            {
+                case 1:
+                    DisplayEventMarketingMessages("Lecture Event", lectureEvent);
+                    break;
+                case 2:
+                    DisplayEventMarketingMessages("Reception Event", receptionEvent);
+                    break;
+                case 3:
+                    DisplayEventMarketingMessages("Outdoor Gathering Event", outdoorEvent);
+                    break;
+                default:
+                    break;
+            }
+
+            Console.WriteLine("\nPress Enter to return to the main menu...");
+            Console.ReadLine();
+        } while (true);
+    }
+
+    // Helper method to display marketing messages for an event
+    static void DisplayEventMarketingMessages(string eventType, Event eventObj)
+    {
+        Console.WriteLine($"{eventType} Marketing Messages:\n");
+        Console.WriteLine($"Standard Details:\n{eventObj.GetStandardDetails()}\n");
+        Console.WriteLine($"Full Details:\n{eventObj.GetFullDetails()}\n");
+        Console.WriteLine($"Short Description:\n{eventObj.GetShortDescription()}\n");
     }
 }
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,279 @@
 using System;
+using System.Collections.Generic;
+
+// Base Activity class
+class Activity
+{
+    private DateTime date;
+    private int minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        this.date = date;
+        this.minutes = minutes;
+    }
+
+    public int GetMinutes()
+    {
+        return minutes;
+    }
+
+    public virtual double GetDistance()
+    {
+        return 0; // Default implementation for activities without distance
+    }
+
+    public virtual double GetSpeed()
+    {
+        return 0; // Default implementation for activities without speed
+    }
+
+    public virtual double GetPace()
+    {
+        return 0; // Default implementation for activities without pace
+    }
+
+    public virtual string GetSummary()
+    {
+        return $"{date.ToShortDateString()} - {GetType().Name} ({minutes} min)";
+    }
+}
+
+// Running class derived from Activity
+class Running : Activity
+{
+    private double distance;
+
+    public Running(DateTime date, int minutes, double distance)
+        : base(date, minutes)
+    {
+        this.distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return distance / (GetMinutes() / 60);
+    }
+
+    public override double GetPace()
+    {
+        return GetMinutes() / distance;
+    }
+
+    public override string GetSummary()
+    {
+        return $"{base.GetSummary()} - Distance: {distance} miles, Speed: {GetSpeed()} mph, Pace: {GetPace()} min per mile";
+    }
+}
+
+// StationaryBicycle class derived from Activity
+class StationaryBicycle : Activity
+{
+    private double speed;
+
+    public StationaryBicycle(DateTime date, int minutes, double speed)
+        : base(date, minutes)
+    {
+        this.speed = speed;
+    }
+
+    public override double GetSpeed()
+    {
+        return speed;
+    }
+
+    public override double GetPace()
+    {
+        return 60 / speed;
+    }
+
+    public override string GetSummary()
+    {
+        return $"{base.GetSummary()} - Speed: {speed} kph, Pace: {GetPace()} min per km";
+    }
+}
+
+// Swimming class derived from Activity
+class Swimming : Activity
+{
+    private int laps;
+
+    public Swimming(DateTime date, int minutes, int laps)
+        : base(date, minutes)
+    {
+        this.laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return laps * 50 / 1000; // Convert laps to kilometers
+    }
+
+    public override double GetSpeed()
+    {
+        return GetDistance() / (GetMinutes() / 60);
+    }
+
+    public override double GetPace()
+    {
+        return GetMinutes() / GetDistance();
+    }
+
+    public override string GetSummary()
+    {
+        return $"{base.GetSummary()} - Distance: {GetDistance()} km, Speed: {GetSpeed()} kph, Pace: {GetPace()} min per km";
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>();
+
+        do
+        {
+            Console.Clear(); // Clear the console before displaying the menu
+
+            Console.WriteLine("Fitness Center Activity Tracker\n");
+            Console.WriteLine("1. Record Running Activity");
+            Console.WriteLine("2. Record Stationary Bicycle Activity");
+            Console.WriteLine("3. Record Swimming Activity");
+            Console.WriteLine("4. View Activity Summaries");
+            Console.WriteLine("5. Exit");
+
+            int choice;
+            do
+            {
+                Console.Write("Enter your choice (1-5): ");
+            } while (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 5);
+
+            Console.Clear(); // Clear the console before displaying the chosen option
+
+            switch (choice)
+            {
+                case 1:
+                    RecordRunningActivity(activities);
+                    break;
+                case 2:
+                    RecordStationaryBicycleActivity(activities);
+                    break;
+                case 3:
+                    RecordSwimmingActivity(activities);
+                    break;
+                case 4:
+                    ViewActivitySummaries(activities);
+                    Console.WriteLine("\nPress Enter to return to the main menu...");
+                    Console.ReadLine();
+                    break;
+                case 5:
+                    Environment.Exit(0); // Exit the program
+                    break;
+                default:
+                    break;
+            }
+
+        } while (true);
+    }
+
+    static void RecordRunningActivity(List<Activity> activities)
+    {
+        Console.WriteLine("Record Running Activity\n");
+
+        DateTime date = GetDate();
+        int minutes = GetMinutes();
+        double distance = GetDoubleInput("Enter the distance (miles): ");
+
+        Running runningActivity = new Running(date, minutes, distance);
+        activities.Add(runningActivity);
+
+        Console.WriteLine("\nRunning activity recorded successfully!");
+    }
+
+    static void RecordStationaryBicycleActivity(List<Activity> activities)
+    {
+        Console.WriteLine("Record Stationary Bicycle Activity\n");
+
+        DateTime date = GetDate();
+        int minutes = GetMinutes();
+        double speed = GetDoubleInput("Enter the speed (kph): ");
+
+        StationaryBicycle bicycleActivity = new StationaryBicycle(date, minutes, speed);
+        activities.Add(bicycleActivity);
+
+        Console.WriteLine("\nStationary bicycle activity recorded successfully!");
+    }
+
+    static void RecordSwimmingActivity(List<Activity> activities)
+    {
+        Console.WriteLine("Record Swimming Activity\n");
+
+        DateTime date = GetDate();
+        int minutes = GetMinutes();
+        int laps = GetIntInput("Enter the number of laps: ");
+
+        Swimming swimmingActivity = new Swimming(date, minutes, laps);
+        activities.Add(swimmingActivity);
+
+        Console.WriteLine("\nSwimming activity recorded successfully!");
+    }
+
+    static void ViewActivitySummaries(List<Activity> activities)
+    {
+        Console.WriteLine("Activity Summaries\n");
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
+    }
+
+    static DateTime GetDate()
+    {
+        DateTime date;
+        do
+        {
+            Console.Write("Enter the date (MM/dd/yyyy): ");
+        } while (!DateTime.TryParse(Console.ReadLine(), out date));
+
+        return date;
+    }
+
+    static int GetMinutes()
+    {
+        int minutes;
+        do
+        {
+            Console.Write("Enter the duration in minutes: ");
+        } while (!int.TryParse(Console.ReadLine(), out minutes) || minutes <= 0);
+
+        return minutes;
+    }
+
+    static int GetIntInput(string prompt)
+    {
+        int value;
+        do
+        {
+            Console.Write(prompt);
+        } while (!int.TryParse(Console.ReadLine(), out value));
+
+        return value;
+    }
+
+    static double GetDoubleInput(string prompt)
+    {
+        double value;
+        do
+        {
+            Console.Write(prompt);
+        } while (!double.TryParse(Console.ReadLine(), out value));
+
+        return value;
     }
 }
